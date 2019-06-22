b62e237b3337d20d7e167898eba91274db310b17
Admin <Admin@LABCA408-PC27.labhard.urp.edu.pe>
2019-06-22 16:21:00 -0500

    python and R

---
+++ Script/Lab_ModelosRegresionAvanzados.R
@@ -0,0 +1,26 @@
+rm(list=ls())
+##########################################################################
+##### -- Programa de Especialización en Data Science - Nivel I -- ########
+##########################################################################
+######## Tema : Modelos de Regresion Avanzados ###########################
+######## Autores: Jose Cardenas - Andre Chavez  ########################## 
+##########################################################################
+############################################
+# ANÁLISIS DE REGRESION SIMPLE Y MULTIPLE #
+############################################
+###################################
+## Ejercicio de Aplicación N° 01 ##
+###################################
+
+# Objetivos del taller:
+# 1.- Aplicar lo desarrollado en las clases teórico prácticas
+# 2.- Plantearse interrogantes respecto a la extensión del modelo lineal.
+
+# Trabajo :
+# Desarrolle un análisis de regresión lineal simple con la siguiente data,como mínimo 
+# debe tener lo siguiente:
+
+#a) Análisis exploratorio previo, es decir justificación de la aplicación de LR.
+#b) Ajuste del modelo de regresión lineal.
+#c) Interpretación de resultados.
+
